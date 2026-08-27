c5b52cd3ceda736035546c00088c4e86832b88dc
Shoaib <shoaibtasrif326@gmail.com>
2026-08-27 13:18:15 +0600

    [uploads]

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -34,9 +34,11 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
 	},
 	upload: {
 		config: {
-			security: {
-				allowedTypes: allowedMediaTypes,
-				deniedTypes: deniedExecutableTypes,
+			provider: "cloudinary",
+			providerOptions: {
+				cloud_name: env("CLOUDINARY_CLOUD_NAME"),
+				api_key: env("CLOUDINARY_API_KEY"),
+				api_secret: env("CLOUDINARY_API_SECRET"),
 			},
 		},
 	},
