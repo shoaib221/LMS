01126718d07b676e4648858a1d70d0edfbc1fcd3
Shoaib <shoaibtasrif326@gmail.com>
2026-08-31 06:35:42 +0600

    [api test]

---
+++ frontend/frontend/.next/dev/static/chunks/_1isf6nv._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/_1u0nyzb._.js","static/chunks/_14sorkl._.js"],
+    source: "entry"
+});
